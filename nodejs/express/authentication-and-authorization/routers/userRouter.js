d98ed516633ab8b6ff1e0102b8f136b323b83ece
callmefarad <ubani.udochukwu@gmail.com>
2023-07-09 13:48:29 +0100

    added comment and user sign-out

--- nodejs/express/authentication-and-authorization/routers/userRouter.js
+++ nodejs/express/authentication-and-authorization/routers/userRouter.js
@@ -1,12 +1,15 @@
 const express = require( 'express' );
 const router = express.Router();
-const { signUp, signIn } = require( '../controllers/userController' );
+const { signUp, signIn, signOut } = require( '../controllers/userController' );
 
 router.route( "/users/sign-up" )
     .post( signUp )
     
 router.route( "/users/sign-in" )
-    .post(signIn)
+    .post( signIn )
+    
+router.route( "/users/sign-out" )
+    .post(signOut)
     
 
 module.exports = router;
