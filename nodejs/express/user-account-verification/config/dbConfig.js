f24c4c779189e5444ac7cce91b611d3f17939c88
callmefarad <ubani.udochukwu@gmail.com>
2023-07-14 16:25:37 +0100

    remove user-email-verification

--- nodejs/express/user-account-verification/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-require( 'dotenv' ).config();
-const mongoose = require( 'mongoose' );
-
-const url = process.env.DATABASE_URI
-mongoose.connect( url ).then( () => {
-    console.log("Database connected successfully.")
-} ).catch( (error) => {
-    console.error("Error connecting to database", error.message)
-});
