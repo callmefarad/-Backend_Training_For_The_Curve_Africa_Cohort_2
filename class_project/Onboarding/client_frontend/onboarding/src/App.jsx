a347e02288fc7d8c627296f8e2debee5968d1a04
callmefarad <ubani.udochukwu@gmail.com>
2023-07-23 14:52:58 +0100

    added forgot password, reset password and change password

---
+++ class_project/Onboarding/client_frontend/onboarding/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import RegistrationForm from './components/registration/Registration';
+function App() {
+
+  return (
+    <>
+      <RegistrationForm />
+    </>
+  )
+}
+
+export default App
---
+++ class_project/Onboarding/client_frontend/onboarding/src/components/registration/Registration.jsx
@@ -0,0 +1,72 @@
+// import { useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+// import axios from "axios";
+// import Swal from "sweetalert2";
+// import "sweetalert2/dist/sweetalert2.css";
+
+export const RegistrationForm = () => {
+    // const [ email, setEmail ] = useState( '' );
+    // const [ password, setPassword ] = useState( '' );
+    // const [ username, setUsername ] = useState( '' );
+    // const [ role, setRole ] = useState( '' );
+    // const [ emailError, setEmailError ] = useState( '' );
+    // const [ passwordError, setPasswordError ] = useState( '' );
+    // const [ usernameError, setUsernameError ] = useState( '' );
+    // const [ roleError, setRoleError ] = useState( '' );
+    // const [ loading, setLoading ] = useState( false );
+
+    // const navigate = useNavigate();
+    
+    // for 11 digit phone number;
+    // const validatePhoneNumber = ( phone ) => {
+    //     phoneRegExp = /^\d{11}$/;
+    //     return phoneRegExp.test( phone );
+    // };
+
+    // // function for submitting form
+    // const handleSubmit = async ( e ) => {
+    //     // prevent page from auto reloading
+    //     e.preventDefault();
+
+    //     // reset previous error message
+    //     setRoleError( "" );
+    //     setUsernameError( "" );
+    //     setPasswordError( "" );
+    //     setEmailError( "" );
+
+    //     // initialize general error handler
+    //     let hasError = false;
+    //     // check for empty fields
+    //     if ( email.trim() === "" ) {
+    //         setEmailError( "Email is required" );
+    //         hasError = true;
+    //     }
+    //     if ( password.trim() === "" ) {
+    //         setNumberError( "Passwrod is required" );
+    //         hasError = true;
+    //     }
+    //     if ( username.trim() === "" ) {
+    //         setUsernameError( "username is required" );
+    //         hasError = true;
+    //     }
+    //     if ( role.trim() === "" ) {
+    //         setRoleError( "role is required" );
+    //         hasError = true;
+    //     }
+
+    //     if ( hasError ) {
+    //         Swal.fire( {
+    //             position: "center-center",
+    //             icon: "error",
+    //             title: "Oops...",
+    //             text: "Fill all fields",
+    //             showConfirmButton: true,
+    //         } );
+    //         setLoading( false );
+    //     }
+    return (
+        <>
+            form will be here
+        </>
+    );
+};
